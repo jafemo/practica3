45911455462023c6a36ac20cfcf3adb641ff3209
jafemo <montajavi@gmail.com>
2016-09-22 09:59:28 +0200

    corregido

--- index.php
+++ index.php
@@ -1,13 +1,16 @@
+<?php
 //fichero index.php
 //creado por Paco Gomez
+?>
 <html>
   <head>
       <title>ACTIVIDAD 03</title>
-      <?php
-        echo "Aquí poner vuestro nombre;
-        echo "Aquí poner vuestro primer apellido";
-        print "Aquí poner vuestro segundo apellido";
-      ?>
   </head>
   <body>
+   <?php
+        echo "Javi <br>";
+        echo "Montañez <br>";
+        print "Fernandez <br>";
+      ?>
+  </body>
 </html>
